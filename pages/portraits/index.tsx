e5f950607b73690a2886eca18b1de4753f50a6ac
Kurt Obando <kurtobando.developer@gmail.com>
2021-07-07 22:30:47 +0800

    add pagination in /portraits page

--- pages/portraits/index.tsx
+++ pages/portraits/index.tsx
@@ -1,17 +1,36 @@
+import { useEffect, useState } from "react"
 import Link from "next/link"
 import Head from "next/head"
 import { GetStaticProps } from "next"
 import { motion } from "framer-motion"
 import Layout from "../../components/Layout"
 import ImageContainer from "../../components/ImageContainer"
+import PaginationLink from "../../components/PaginationLink"
+import Pagination from "../../lib/pagination"
 import fetchAllPortraits from "../../lib/fetchAllPortraits"
 import { SITE_TITLE } from "../../config/constant"
 
-interface Props {
-    data: any
-}
+const paginate = new Pagination()
 
-export default function Portraits({ data }: Props): JSX.Element {
+export default function Portraits({ data }: { data: any }): JSX.Element {
+    const [pageTotal, setPageTotal] = useState<number>(0)
+    const [pageCurrent, setPageCurrent] = useState<number>(1)
+    const [pageData, setPageData] = useState<any[]>([])
+
+    useEffect(() => {
+        paginate.pageData = data
+        paginate.pagePer = 9
+        paginate.pageCurrent = pageCurrent
+        paginate.exec()
+
+        setPageData(paginate.pageData)
+        setPageTotal(paginate.pageTotal)
+    }, [pageCurrent])
+
+    const onClickPagination = (n: number) => {
+        setPageCurrent(n)
+    }
+
     return (
         <>
             <Layout>
@@ -20,7 +39,7 @@ export default function Portraits({ data }: Props): JSX.Element {
                 </Head>
                 <motion.div key="portraits" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
                     <div className="flex flex-row flex-wrap mt-2">
-                        {data.map((portrait) => {
+                        {pageData.map((portrait) => {
                             return (
                                 <div key={portrait?.node?.id} className="w-full md:w-1/2 lg:w-1/3 p-1">
                                     <Link href={`/portraits/${portrait?.node?.slug}`}>
@@ -36,6 +55,9 @@ export default function Portraits({ data }: Props): JSX.Element {
                         })}
                     </div>
                 </motion.div>
+                <div className="flex flex-row flex-wrap justify-center my-8">
+                    <PaginationLink pageTotal={pageTotal} pageCurrent={pageCurrent} onClick={onClickPagination} />
+                </div>
             </Layout>
         </>
     )
